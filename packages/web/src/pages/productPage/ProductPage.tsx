1a26ac8dd7aec389a4eaf82c34404fa6d04e60fd
Sebastian <sebastian.gonzalez211@inacapmail.cl>
2023-06-01 17:21:24 -0400

    productList changes

--- packages/web/src/pages/productPage/ProductPage.tsx
+++ packages/web/src/pages/productPage/ProductPage.tsx
@@ -1,13 +1,6 @@
 import { httpsCallable } from "firebase/functions";
 import { Card, CardContent, Typography } from "@mui/material";
 import { functions } from "@feria-a-ti/common/firebase";
-import {
-  Key,
-  ReactElement,
-  JSXElementConstructor,
-  ReactFragment,
-  ReactPortal,
-} from "react";
 
 const productListFunction = httpsCallable(functions, "productList");
 productListFunction()
@@ -15,62 +8,20 @@ productListFunction()
     const products = result.data as any;
 
     // se generan las tarjetas con las frutas o verduras
-    const productCards = products.map(
-      (product: {
-        id: Key | null | undefined;
-        name:
-          | string
-          | number
-          | boolean
-          | ReactElement<any, string | JSXElementConstructor<any>>
-          | ReactFragment
-          | ReactPortal
-          | null
-          | undefined;
-        image:
-          | string
-          | number
-          | boolean
-          | ReactElement<any, string | JSXElementConstructor<any>>
-          | ReactFragment
-          | ReactPortal
-          | null
-          | undefined;
-        description:
-          | string
-          | number
-          | boolean
-          | ReactElement<any, string | JSXElementConstructor<any>>
-          | ReactFragment
-          | ReactPortal
-          | null
-          | undefined;
-        price:
-          | string
-          | number
-          | boolean
-          | ReactElement<any, string | JSXElementConstructor<any>>
-          | ReactFragment
-          | ReactPortal
-          | null
-          | undefined;
-      }) => (
-        <Card key={product.id}>
-          <CardContent>
-            <Typography variant="h5" component="div">
-              {product.name}
-            </Typography>
-            <Typography color="text.secondary">{product.image}</Typography>
-            <Typography color="text.secondary">
-              {product.description}
-            </Typography>
-            <Typography color="text.secondary">{product.price}</Typography>
-          </CardContent>
-        </Card>
-      )
-    );
+    const productCards = products.map((product: any) => (
+      <Card key={product.id}>
+        <CardContent>
+          <Typography variant="h5" component="div">
+            {product.name}
+          </Typography>
+          <Typography color="text.secondary">{product.image}</Typography>
+          <Typography color="text.secondary">{product.description}</Typography>
+          <Typography color="text.secondary">{product.price}</Typography>
+        </CardContent>
+      </Card>
+    ));
 
-    // Renderizar las tarjetas en tu página
+    // retorna las cards con los productos
     return <div>{productCards}</div>;
   })
   .catch((error) => {
